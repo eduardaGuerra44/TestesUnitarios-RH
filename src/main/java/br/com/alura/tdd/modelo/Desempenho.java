9fa9d44d31e1f4ea18c26651411f0f726a5fe7ff
Eduarda Guerra <eduardaguerra44@gmail.com>
2024-01-08 10:02:48 -0300

    Refatoração ReajusteService

--- src/main/java/br/com/alura/tdd/modelo/Desempenho.java
+++ src/main/java/br/com/alura/tdd/modelo/Desempenho.java
@@ -1,5 +1,25 @@
 package br.com.alura.tdd.modelo;
+import java.math.BigDecimal;
 
 public enum Desempenho {
-    A_Desejar, Bom, Otimo;
+    A_Desejar {
+        @Override
+        public BigDecimal percentualReajuste() {
+            return new BigDecimal("0.03");
+        }
+    },
+    Bom {
+        @Override
+        public BigDecimal percentualReajuste() {
+            return new BigDecimal("0.15");
+        }
+    },
+    Otimo {
+        @Override
+        public BigDecimal percentualReajuste() {
+            return new BigDecimal("0.20");
+        }
+    };
+
+    public abstract BigDecimal percentualReajuste();
 }
